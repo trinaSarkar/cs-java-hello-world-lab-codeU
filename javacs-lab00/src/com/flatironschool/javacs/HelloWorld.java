9f1c4ff4fe0a27e7bde78ed630edae8b7401e862
trinaSarkar <tsarkar@stanford.edu>
2016-04-03 12:55:04 -0700

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,11 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        return Double.parseDouble((System.getProperty("java.specification.version")));
+
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    System.out.println(getVersion());
     }
 }
